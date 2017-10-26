cebc1b060d8e0ab35a32ee3109b1121b1543e646
Levindo Neto <levindogtn@gmail.com>
2017-10-26 02:37:50 +0200

    Add initial files for the task 1

---
+++ Assignment-1/gbs-ws1718/gbsh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	fprintf(stdout, "Hello World!\n");
+
+	// shell code here
+	
+	// ...
+	
+	// ...
+
+	exit(0); // exit normally	
+}
