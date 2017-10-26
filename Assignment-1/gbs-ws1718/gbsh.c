09ec3cc99c479bb6223072235fa2a7295ec903f1
Levindo Neto <levindogtn@gmail.com>
2017-10-27 01:48:22 +0200

    Add basic prompt, Add header

--- Assignment-1/gbs-ws1718/gbsh.c
+++ Assignment-1/gbs-ws1718/gbsh.c
@@ -1,15 +1,39 @@
+/* Shell GBSH Development
+ * Features: prompt, exit, pwd, sum-max
+ * Members: Leonardo Nahra, Levindo Neto and Li Smile
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "gbsh.h"
+
+#define EXIT "exit"
+
+void prompt(char *statement, char **command) {
+    printf("%s", statement);
+    printf("THIS IS THE COMMAND: %s\n", *command);
+    for(int i = 0; 1; i++) {
+        if(i) {
+            *command = (char*)realloc((*command),i+1);
+        }
+        else {
+            *command = (char*)malloc(i+1);
+        }
+        (*command)[i] = getchar();
+        if((*command)[i] == '\n') {
+            (*command)[i] = '\0';
+            break;
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
-	
-	fprintf(stdout, "Hello World!\n");
+    char *command = NULL;
+    do {
+        prompt("Enter the String:- ", &command);
 
-	// shell code here
-	
-	// ...
-	
-	// ...
-
-	exit(0); // exit normally	
+    } while(strcmp(EXIT,command) != 0); // keep running unless the user types "exit"
+    free(command);
+    printf("Out...");
+    return 0; // Exit the shell
 }
---
+++ Assignment-1/gbs-ws1718/gbsh.h
@@ -0,0 +1,20 @@
+/**************************** Libraries ***************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/******************************************************************************/
+
+/***************************** Structs ****************************************/
+
+/******************************************************************************/
+
+/************************ Prototypes of used functions ************************/
+
+/* Prompt that waits for an user input
+ * @Parameters: String: Displayed statement before the command (user info)
+                String command (followed by Enter)
+ * @Return: Void, it just shows the statement + command to the user
+ */
+void prompt(char *statement,char **command);
+
+/******************************************************************************/
