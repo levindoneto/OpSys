fee1908f1096af6fbd23152d5a379301f9677a88
Levindo Neto <levindogtn@gmail.com>
2017-10-27 03:07:34 +0200

    Add pc information to the prompt, Add struct for the user info

--- Assignment-1/gbs-ws1718/gbsh.c
+++ Assignment-1/gbs-ws1718/gbsh.c
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <windows.h> // Used for getting the logged user
+#include <Lmcons.h>
 #include "gbsh.h"
 
 #define EXIT "exit"
 
-void prompt(char *statement, char **command) {
-    printf("%s", statement);
+void prompt(UserInfo userInformation, char **command) {
+    printf("<%s>@<%s> <%s> > ", userInformation.user, userInformation.host, userInformation.cwd);
     for(int i = 0; 1; i++) {
         if(i) {
             *command = (char*)realloc((*command),i+1);
@@ -24,16 +28,33 @@ void prompt(char *statement, char **command) {
             break;
         }
     }
-    //printf("%s", *command);
+}
+
+void storeInfo (UserInfo *infoUser) {
+    /* CWD: Current path */
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    infoUser->cwd = cwd;
+    /* Logged username */
+    char username[UNLEN+1];
+    DWORD username_len = UNLEN+1;
+    GetUserName(username, &username_len);
+    infoUser->user = username; //ToDo: Modify for unix implementation
+    /* Logged Host */
+    //char hostname[1024];
+    //gethostname(hostname, 1024);
+    infoUser->host = "host"; //ToDo: Modify for unix implementation
 }
 
 int main(int argc, char *argv[]) {
     char *command = NULL;
+    UserInfo userInformation; /* It doesn't change during the prompt's execution
+                               * user, host, cwd */
+    storeInfo(&userInformation);
     do {
-        prompt("<user>@<host> <cwd> > ", &command);
-        free(command);
+        prompt(userInformation, &command);
     } while(strcmp(EXIT,command) != 0); // keep running unless the user types "exit"
     free(command);
-    printf("Out...");
+
     return 0; // Exit the shell
 }
--- Assignment-1/gbs-ws1718/gbsh.h
+++ Assignment-1/gbs-ws1718/gbsh.h
@@ -2,19 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 /******************************************************************************/
 
 /***************************** Structs ****************************************/
-
+typedef struct UserInformation {
+    char *user;
+    char *host;
+    char *cwd;
+} UserInfo;
 /******************************************************************************/
 
 /************************ Prototypes of used functions ************************/
 
 /* Prompt that waits for an user input
- * @Parameters: String: Displayed statement before the command (user info)
+ * @Parameters: UserInfo Struct: user, host and cwd information
                 String command (followed by Enter)
  * @Return: Void, it just shows the statement + command to the user
  */
-void prompt(char *statement,char **command);
+void prompt(UserInfo userInformation,char **command);
+
+/* Prompt that waits for an user input
+ * @Parameters: Struct UserInfo
+ * @Return: Void, it just stores data on the UserInfo struct (user, host, cwd)
+ */
+void storeInfo (UserInfo *infoUser);
 
 /******************************************************************************/
