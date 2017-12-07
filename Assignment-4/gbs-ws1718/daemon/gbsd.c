fc3c5b6c66e554ac6bfb863600e6815f0226563d
Leonardo Nahra <leonardonahra@gmail.com>
2017-12-07 18:37:21 +0100

    Add daemon

--- Assignment-4/gbs-ws1718/daemon/gbsd.c
+++ Assignment-4/gbs-ws1718/daemon/gbsd.c
@@ -1,8 +1,15 @@
+#include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-void log_message(char *filename, const char* format, ...) {
+static const char *LOGFILE = "/tmp/gbsd.log";
+
+void log_message(const char *filename, const char* format, ...) {
     FILE *logfile;
     logfile = fopen(filename, "a");
 
@@ -19,13 +26,48 @@ void log_message(char *filename, const char* format, ...) {
 }
 
 int main(int argc, char *argv[]) {
-    fprintf(stdout, "Hello World!\n");
 
-    // daemon code here
+    // fork off the parent process
+    pid_t pid = fork();
 
-    // ...
+    if (pid < 0) {
+        printf("%s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
 
-    // ...
+    // change the file mode mask;
+    umask(0);
+
+    // create new SID
+    pid_t sid = setsid();
+
+    if (sid < 0) {
+        // TODO log error
+        exit(EXIT_FAILURE);
+    }
+
+    // change directory to root
+    if (chdir("/") < 0) {
+        // TODO log error
+        exit(EXIT_FAILURE);
+    }
+
+    // close standard file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    // daemon
+    pid_t dpid = getpid();
+    int seconds = 0;
+    while (1) {
+        log_message(LOGFILE, "[STATUS] PID %d - %d", dpid, seconds);
+        seconds++;
+        sleep(1);
+    }
 
     return 0;
 }
